3d3bf8b79b3695dee2e2dbda067d45359ba96686
sdcro <sdcrouse91@gmail.com>
2019-09-06 15:09:45 -0700

    Edited UNITS_LIST in Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,9 +2,9 @@ class Ingredient < ActiveRecord::Base
   belongs_to :recipe
 
   UNITS_LIST = {
-    :us => ["cup", "fl oz", "gal", "lb", "oz", "pt", "qt", "tbsp", "tsp"],
-    :si => ["L", "g", "kL", "kg", "mL", "mg"],
-    :other => ["dash", "drop", "drops", "handful", "handfuls", "piece", "pieces", "pinch"]
+    :us => ["cup", "cups", "fl oz", "gal", "lb", "oz", "pt", "qt", "tbsp", "tsp"],
+    :si => ["g", "kg", "kL", "L", "mg", "mL"],
+    :other => ["dash", "drop", "drops", "handful", "handfuls", "piece", "pieces", "pinch", "scoop", "scoops"]
   }
   # Note: When the user chooses a unit from the :other hash, that unit should have the word "of" after it in the view.
   # That, or just have "of" after EVERY unit.
