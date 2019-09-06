0a127c3529368945e23e6ce461b06b11c1415ab5
sdcro <sdcrouse91@gmail.com>
2019-09-05 17:43:22 -0700

    Revert "Added units list to Ingredient model"
    This reverts commit 99875cdd9f3a2f9b1221225d45101abaf2c49ec7.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,11 +1,3 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
-
-  @@units_list = [
-    {:us => ["cup", "fl oz", "gal", "lb", "oz", "pt", "qt", "tbsp", "tsp"]},
-    {:si => ["L", "g", "kL", "kg", "mL", "mg"],
-    {:other => ["dash", "drop", "drops", "handful", "handfuls", "piece", "pieces", "pinch"]}
-    # Note: When the user chooses a unit from the :other hash, that unit should have the word "of" after it in the view.
-    # That, or just have "of" after EVERY unit.
-  ]
 end
