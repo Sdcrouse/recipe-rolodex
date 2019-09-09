2a2d7635c5a127246431797fb3aa19beeef2a788
sdcro <sdcrouse91@gmail.com>
2019-09-09 00:01:53 -0700

    Add required fields to signup form, and create a user profile page and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   post '/users' do
-    "You have made a new user!"
+    new_user = User.new(params)
+    # I need a validation or two here - don't create a blank user!
+    new_user.save
+
+    # I want to use a #slug method here, and redirect to '/users/slug' (or something similar)
+    redirect to "/users/#{new_user.id}"
+  end
+
+  get '/users/:id' do
+    @user = User.find_by_id(params[:id])
+    erb :"users/profile"
   end
 end
