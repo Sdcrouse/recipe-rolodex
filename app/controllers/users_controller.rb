2a11164bd897aa4bf94032a45f43badfe16630e6
sdcro <sdcrouse91@gmail.com>
2019-09-09 15:44:43 -0700

    Add logout feature to UsersController, and test it out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,27 @@ class UsersController < ApplicationController
     redirect to "/users/#{new_user.id}"
   end
 
+  get '/users/logout' do
+    session.clear
+    redirect to "/"
+  end
+
   get '/users/:id' do
     @user = User.find_by_id(params[:id])
     erb :"users/profile"
   end
+
+  # Later on, I want to render a logout.erb page that confirms the user's choice to log out.
+  # That would require a get '/users/:id/logout' route (and I would need to update the corresponding <a> on the layout page).
+  # If they say "No", then redirect them to their profile page.
+  # This will be easier to implement (I think), once the login and logout links are only shown when the user is respectively logged out or in.
+
+  # get '/users/:id/logout' do
+  #   @user = User.find_by_id(params[:id])
+  #   erb :"users/logout"
+  # end
+
+  # post '/users/logout' do
+  #   "You have successfully logged out!" # This could be a flash message.
+  # end
 end
