53212f71e32da0af3360b9f30dfbeda2f79f0c8b
sdcro <sdcrouse91@gmail.com>
2019-09-10 01:15:15 -0700

    Update the database with new recipes, and sort the list of recipes by name and username

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,9 @@ tea = Ingredient.create(quantity: "one", units: "cup", name: "tea")
 sausages = Ingredient.create(quantity: "5", name: "sausage links")
 pancakes = Ingredient.create(quantity: "3", name: "pancakes")
 
+pipeweed = Ingredient.create(quantity: "2", units: "pinches", name: "pipeweed")
+shire_bread = Ingredient.create(quantity: "2", units: "slices", name: "Shire wheat bread")
+
 second_breakfast = Recipe.create(
   name: "Second Breakfast",
   description: "The meal every hobbit worth his weight should know how to make!",
@@ -32,13 +35,24 @@ second_breakfast = Recipe.create(
   instructions: "Prepare all dishes mid-morning. Serve warm at any time between breakfast and lunch."
 )
 
+shire_idiot_sandwich = Recipe.create(
+  name: "Idiot Sandwich",
+  ingredients: [pipeweed, shire_bread],
+  instructions: "Sprinkle the pipeweed on one slice of bread. Cover with the other slice. Eat it at your own risk.",
+  notes: "Only a fool would make this! Pipeweed is meant to be smoked, not eaten!"
+)
+
 bilbo_baggins.recipes << second_breakfast
+bilbo_baggins.recipes << shire_idiot_sandwich
 
 #-------- Egghead's recipe(s) -------------------------
 eggs = Ingredient.create(quantity: "3", name: "eggs")
 salt = Ingredient.create(quantity: "A dash", name: "salt")
 pepper = Ingredient.create(quantity: "A dash", name: "pepper")
 chipotle_sauce = Ingredient.create(quantity: "10-15", units: "drops", name: "Chipotle Tabasco Sauce")
+
+rotten_egg = Ingredient.create(quantity: "1", name: "rotten egg")
+moldy_bread = Ingredient.create(quantity: "2", units: "slices", name: "moldy bread")
 
 smokey_eggs = Recipe.create(
   name: "Smokey Scrambled Eggs",
@@ -48,7 +62,16 @@ smokey_eggs = Recipe.create(
   instructions: "Beat all ingredients in a bowl with a whisk or fork, about two minutes. Place a frying pan on a stove at low to medium heat. Pour eggs into frying pan. Stir constantly for about three minutes, or until eggs begin to turn brown. Serve immediately."
 )
 
+dangerous_sandwich = Recipe.create(
+  name: "Idiot Sandwich",
+  description: "DO NOT MAKE THIS!!! This is an example of what not to do!",
+  serving_size: "One nasty sandwich", servings: "1 (or zero, if you're wise)",
+  ingredients: [rotten_egg, moldy_bread],
+  instructions: "Put a clothespin on your nose and gloves on your hands. Crack rotten egg over one slice of bread, then cover with the other slice. Throw it out, unless you want to go to the hospital!"
+)
+
 egghead.recipes << smokey_eggs
+egghead.recipes << dangerous_sandwich
 
 #----------- Winco's Recipes ----------------------------
 winco_ingredients = {
@@ -71,6 +94,12 @@ winco_ingredients = {
     {quantity: "2", name: "eggs"},
     {quantity: "1", units: "teaspoon", name: "dried parsley"},
     {quantity: "1", units: "tablespoon", name: "dried minced onion"}
+  ],
+  idiot_sandwich: [
+    {quantity: "2", units: "slices", name: "bread"},
+    {quantity: "2", name: "dill pickle spears"},
+    {quantity: "1", units: "large scoop", name: "chocolate ice cream"},
+    {quantity: "1", units: "whole", name: "habanero pepper, quartered"}
   ]
 }
 
@@ -96,6 +125,14 @@ winco_recipes = {
     ingredients: winco_ingredients[:egg_drop_soup].collect{|ingredient| Ingredient.create(ingredient)},
     additional_ingredients: "1 tablespoon cornstarch",
     instructions: "In a medium saucepan, combine water, bouillon, and parsley and onion flakes. Bring to a boil. Lightly beat eggs together. Gradually stir into soup. Remove about half a cup of the soup. Stir in cornstarch until there are no lumps, and return to the soup. Boil until soup thickens."
+  },
+
+  idiot_sandwich: {
+    name: "idiot sandwich",
+    description: "Disclaimer: This is NOT an official Winco Foods recipe! Apologies to Winco and its affiliated companies.",
+    ingredients: winco_ingredients[:idiot_sandwich].collect{|ingredient| Ingredient.create(ingredient)},
+    instructions: "Plop the chocolate ice cream on one slice of bread. Place the dill pickle spears on top of the ice cream. Place the quartered habanero on top of that. Cover with the other slice of bread. Refrigerate overnight. Serves one unfortunate victim.",
+    notes: "This is (probably) edible, but we pity your tastebuds and stomach if you're brave (or crazy) enough to try it."
   }
 }
 
@@ -103,5 +140,5 @@ winco_recipes_array = winco_recipes.collect do |recipe_name, recipe_hash|
   Recipe.create(recipe_hash)
 end
 
-winco_recipes_array.each{|recipe| winco.recipes << recipe} # This isn't working, somehow.
+winco_recipes_array.each{|recipe| winco.recipes << recipe}
 # Either that ^^^, or set winco.recipes = winco_recipes_array, then save winco
