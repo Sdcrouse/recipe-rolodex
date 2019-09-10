104b88167c89aefe973c5b15e13d0674a4c6cb5c
sdcro <sdcrouse91@gmail.com>
2019-09-09 23:26:56 -0700

    Create and test the RecipesController

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,7 @@
+class RecipesController < ApplicationController
+  # Don't forget to use Rack::MethodOverride in the config.ru file. I may need to require it here as well.
+
+  get '/recipes' do
+    "Testing the RecipesController"
+  end
+end
