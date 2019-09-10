600a917fc1096ceb52bf8fa844986e4b1a06321d
sdcro <sdcrouse91@gmail.com>
2019-09-09 23:57:31 -0700

    Add /recipes route and its corresponding index.erb file

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,8 @@ class RecipesController < ApplicationController
   # Don't forget to use Rack::MethodOverride in the config.ru file. I may need to require it here as well.
 
   get '/recipes' do
-    "Testing the RecipesController"
+    # Users should not access this unless logged in, either by clicking on a link (that should be hidden) or by entering the URL.
+    @recipes = Recipe.all
+    erb :"recipes/index"
   end
 end
