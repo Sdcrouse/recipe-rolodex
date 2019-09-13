a751641d39c10457c29ce5af95d8e2cc61bc2f53
sdcro <sdcrouse91@gmail.com>
2019-09-12 23:11:13 -0700

    Prevent users from viewing all of the recipes unless logged in
    I also added this minor edit: greeting the users by name when there are no recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,9 +2,12 @@ class RecipesController < ApplicationController
   # Don't forget to use Rack::MethodOverride in the config.ru file. I may need to require it here as well.
 
   get '/recipes' do
-    # Users should not access this unless logged in, either by clicking on a link (that should be hidden) or by entering the URL.
-    @recipes = Recipe.all
-    erb :"recipes/index"
+    if logged_in?
+      @recipes = Recipe.all
+      erb :"recipes/index"
+    else
+      redirect to "/users/login" # Add a flash message here.
+    end
   end
 
   get '/recipes/new' do
