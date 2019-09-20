dca3a3ba1a877874f5224072a00bf1a395921bdc
sdcro <sdcrouse91@gmail.com>
2019-09-19 22:52:59 -0700

    Add 'dependent' option and validations to Recipe model
        When a recipe is destroyed, its corresponding entries in the recipe_ingredients table should be destroyed. The corresponding ingredients should NOT be destroyed.
        A recipe has at least one serving (and 2 servings makes more sense than 2.333 servings), but the servings don't need to be specified.
        A recipe has a name, instructions, and at least one ingredient.
    
        THIS HAS NOT BEEN TESTED! DO NOT ROLLBACK TO THIS COMMIT!

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,12 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy 
+  # Destroying a recipe should destroy its recipe_ingredients ^^^, but not the associated ingredients themselves.
+  # I should test that, making sure that nothing's wonky.
   has_many :ingredients, through: :recipe_ingredients
+  
+  validates :servings, numericality: {only_integer: true, greater_than: 0}
+  validates :name, presence: true
+  validates :ingredients, presence: true # I need to test this one and view the error message. I want the recipe to have at least one ingredient.
+  validates :instructions, presence: true
 end
