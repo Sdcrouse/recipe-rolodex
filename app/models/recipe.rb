8ba95b5a9ce7387528621d39e892b612e8ae9e30
sdcro <sdcrouse91@gmail.com>
2019-09-20 00:05:45 -0700

    Remove validation for servings from Recipe model
    I can always put this validation back later. I removed it due to a humorous servings value in the seeds.rb file that I chose to leave in.
    Check out the dangerous_sandwich (lines 92-97 at the time of this commit).

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,9 +9,8 @@ class Recipe < ActiveRecord::Base
     # If the recipe has no ingredients when it's saved, then AR generates my custom error message.
     # If the recipe has an ingredient that has NOT been saved, then AR generates the message, "Ingredients is invalid", without saying why.
   # To avoid getting the latter error message, I should write my custom flash message: "Ingredient's name can't be blank"
-  
+
   validates :name, presence: true
-  validates :servings, numericality: {only_integer: true, greater_than: 0}, allow_blank: true
   validates :instructions, presence: true
 
   def recipe_should_have_at_least_one_ingredient # Edge case, due to "new recipe" form requiring at least one ingredient with a name.
