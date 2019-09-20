4af70583487476fe56db7689a518eb81269fe64e
sdcro <sdcrouse91@gmail.com>
2019-09-19 23:56:13 -0700

    Test and debug new Recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,19 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :recipe_ingredients, dependent: :destroy 
   # Destroying a recipe should destroy its recipe_ingredients ^^^, but not the associated ingredients themselves.
-  # I should test that, making sure that nothing's wonky.
   has_many :ingredients, through: :recipe_ingredients
   
-  validates :servings, numericality: {only_integer: true, greater_than: 0}
+  validate :recipe_should_have_at_least_one_ingredient
+  # Update: Two possible error messages are generated here:
+    # If the recipe has no ingredients when it's saved, then AR generates my custom error message.
+    # If the recipe has an ingredient that has NOT been saved, then AR generates the message, "Ingredients is invalid", without saying why.
+  # To avoid getting the latter error message, I should write my custom flash message: "Ingredient's name can't be blank"
+  
   validates :name, presence: true
-  validates :ingredients, presence: true # I need to test this one and view the error message. I want the recipe to have at least one ingredient.
+  validates :servings, numericality: {only_integer: true, greater_than: 0}, allow_blank: true
   validates :instructions, presence: true
+
+  def recipe_should_have_at_least_one_ingredient # Edge case, due to "new recipe" form requiring at least one ingredient with a name.
+    errors.add(:recipe, "should have at least one ingredient") if self.ingredients.blank?
+  end
 end
