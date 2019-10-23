38f3e439d097987409afb9c7cc8c44567f77e749
sdcro <sdcrouse91@gmail.com>
2019-10-22 17:17:58 -0700

    Refactor multiple routes in RecipesController with the #redirect_if_not_logged_in helper method, and update comment
    Most, if not all, routes in the RecipesController check to see whether a user is logged in.
    They redirect with an error message if the user is not logged in.
    This has been refactored into a #redirect_if_not_logged_in helper method, which is in the ApplicationController.
    Although these are edge cases, some have been marked as important, because the program will break if a user tries to create, update, or delete a recipe while logged out.
    One of the comments referred to a no-longer-existing comment in the recipe show file; it has been updated accordingly.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,13 +7,10 @@ class RecipesController < ApplicationController
   end
 
   get '/recipes/new' do
-    if logged_in?
-      @ingredients = Ingredient.all
-      erb :"/recipes/new"
-    else
-      flash[:error] = "Sorry, chef! You must be logged in before you can create a new recipe."
-      redirect to "/users/login"
-    end
+    redirect_if_not_logged_in
+    
+    @ingredients = Ingredient.all
+    erb :"/recipes/new"
   end
 
   post '/recipes' do
@@ -21,10 +18,7 @@ class RecipesController < ApplicationController
     # I decided against having a bunch of nested "if" statements, due to the calls to #redirect exiting the route anyway.
     # Later on, I may want to convert blank values to nil with #presence.
 
-    if !logged_in? # This is an edge case.
-      flash[:error] = "You cannot edit this recipe without being logged in."
-      redirect to "/users/login"
-    end
+    redirect_if_not_logged_in("create a recipe") # Important edge case
 
     recipe = Recipe.new(params[:recipe])
     
@@ -63,62 +57,44 @@ class RecipesController < ApplicationController
   end # End of " post '/recipes' " route
 
   get '/recipes/:id' do
-    if logged_in? && @recipe = Recipe.find_by_id(params[:id])
+    redirect_if_not_logged_in
+
+    if @recipe = Recipe.find_by_id(params[:id])
       @recipe_ingredients = @recipe.recipe_ingredients
-      # As noted in the show.erb file, I think this will prevent more database queries than are necessary.
+      # I think this will prevent more database queries than are necessary.
       
       erb :'recipes/show'
-    else
-      # Redirect, if there is no recipe and/or the user is not logged in.
-      # I also accounted for this in the recipes/show.erb file.
-      # Later, I could make a separate error page.
-      
-      if !logged_in?
-        # Odd; I encountered a bug that used the '/recipes/new' flash message instead. But how?
-        # I don't know how to recreate it, but evidently the former flash[:error] message wasn't overwritten.
-        # Update: This may have occurred before I changed the validations in the Recipe model; it likely won't happen now.
+    else # Redirect if there is no recipe.
+      flash[:error] = "Sorry, chef! That recipe doesn't exist."
+      redirect to "/recipes"
+    end # End of "if @recipe..."
 
-        flash[:error] = "You must be logged in before you can view this recipe."
-        redirect to "/users/login"
-      else # The user is logged in, but the recipe does not exist.
-        flash[:error] = "Sorry, chef! That recipe doesn't exist."
-        redirect to "/recipes"
-      end
-    end # End of "if logged_in? && @recipe..."
   end # End of "get '/recipes/:id'" route
 
   get '/recipes/:id/edit' do
-    if logged_in?
-      @recipe = Recipe.find_by_id(params[:id])
+    redirect_if_not_logged_in
 
-      if !@recipe # The recipe does not exist.
-        flash[:error] = "Sorry, Chef #{current_user.username}! The recipe that you are trying to edit does not exist."
-        redirect to "/recipes"
-      elsif @recipe.user != current_user
-        # The recipe exists, but the current_user is not its author (and is thus not allowed to edit it).
-        
-        flash[:error] = "Sorry, Chef #{current_user.username}! You are not authorized to edit this recipe."
-        redirect to "/recipes/#{@recipe.id}"
-      else # The recipe exists and the current_user is allowed to edit it.
-        @all_ingredients = Ingredient.all
-        # My goal here ^^^ is to create 5 ingredient fields in the edit form.
-        # I need to know how many will be blank, based on how many recipe_ingredients are in the original recipe.
+    @recipe = Recipe.find_by_id(params[:id])
 
-        erb :"/recipes/edit"
-      end # End of "if !@recipe"
-    else
-      flash[:error] = "Sorry, Chef! You must be logged in to edit this recipe."
-      redirect to "/users/login"
-    end # End of "if logged_in?"
+    if !@recipe # The recipe does not exist.
+      flash[:error] = "Sorry, Chef #{current_user.username}! The recipe that you are trying to edit does not exist."
+      redirect to "/recipes"
+    elsif @recipe.user != current_user
+      # The recipe exists, but the current_user is not its author (and is thus not allowed to edit it).
+      
+      flash[:error] = "Sorry, Chef #{current_user.username}! You are not authorized to edit this recipe."
+      redirect to "/recipes/#{@recipe.id}"
+    else # The recipe exists and the current_user is allowed to edit it.
+      @all_ingredients = Ingredient.all
+      # My goal here ^^^ is to create 5 ingredient fields in the edit form.
+      # I need to know how many will be blank, based on how many recipe_ingredients are in the original recipe.
+
+      erb :"/recipes/edit"
+    end # End of "if !@recipe"
   end # End of "get '/recipes/:id/edit'" route
 
   patch '/recipes/:id' do
-    if !logged_in? 
-      # This is an edge case. I don't know why it won't work when I clear the session just before calling #logged_in? 
-      # Update: I also have to set the session[:user_id] to nil; clearing the session won't work.
-      flash[:error] = "Congratulations, chef! You just found a bug in the Recipe Rolodex! Either you somehow got this far without being logged in, or you got logged out while editing a recipe."
-      redirect to "/users/login"
-    end
+    redirect_if_not_logged_in("update a recipe") # Important edge case
 
     recipe = Recipe.find_by_id(params[:id])
 
@@ -187,23 +163,20 @@ class RecipesController < ApplicationController
   end
 
   delete '/recipes/:id' do
-    if !logged_in? # Edge case
-      flash[:error] = "You must be logged in to delete this recipe."
-      redirect to "/users/login"
-    else
-      recipe = Recipe.find_by_id(params[:id])
-      
-      if !recipe # Edge case
-        flash[:error] = "This recipe does not exist."
-        redirect to "/recipes"
-      elsif recipe.user != current_user # Edge case ("Delete Recipe" button only shows up for the authorized user)
-        flash[:error] = "Sorry, Chef #{current_user.username}! You are not authorized to delete this recipe."
-        redirect to "/recipes/#{recipe.id}"
-      else # The user is logged in, the recipe exists, and the user is authorized to delete it.
-        recipe.destroy
-        flash[:success] = "You have successfully deleted the recipe!"
-        redirect to "/recipes"
-      end # End of "if !recipe"
-    end # End of "if !logged_in?"
+    redirect_if_not_logged_in("delete a recipe") # Important edge case
+
+    recipe = Recipe.find_by_id(params[:id])
+    
+    if !recipe # Edge case
+      flash[:error] = "This recipe does not exist."
+      redirect to "/recipes"
+    elsif recipe.user != current_user # Edge case ("Delete Recipe" button only shows up for the authorized user)
+      flash[:error] = "Sorry, Chef #{current_user.username}! You are not authorized to delete this recipe."
+      redirect to "/recipes/#{recipe.id}"
+    else # The user is logged in, the recipe exists, and the user is authorized to delete it.
+      recipe.destroy
+      flash[:success] = "You have successfully deleted the recipe!"
+      redirect to "/recipes"
+    end # End of "if !recipe"
   end # End of "delete '/recipes/:id'" route
 end # End of RecipesController
